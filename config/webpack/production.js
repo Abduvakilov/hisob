2cff9185e5917f86332449c5762c320dc0962ac5
Mohinabonu <apple@MacBook-Air.local>
2018-12-02 11:20:03 +0500

    brotli removed

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,7 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-const brotli = require('./brotli')
-environment.config.merge(brotli)
-
 module.exports = environment.toWebpackConfig()
