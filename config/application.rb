cc8eef7ee4b02d6b9273a0dce834e6339aa1fef5
Abduvakilov <abd_dilshod@mail.ru>
2022-04-12 11:00:54 +0500

    upgraded to rails 7 from 6.1

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,12 +14,13 @@ module Acc
     config.autoload_paths += Dir[Rails.root.join("app", "models", "transaction")]
     config.i18n.load_path += Dir[Rails.root.join('locales', '*.uz.yml')]
   	config.i18n.default_locale = :uz
-    # config.after_initialize do
-      # Date::DATE_FORMATS[:default] = I18n.t('date.formats.default')
-    # end
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_062712) do
+ActiveRecord::Schema[6.1].define(version: 2022_04_10_062712) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name", null: false
